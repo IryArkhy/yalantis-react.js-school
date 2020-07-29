5d8c4716e771e88620286fc857ba59eef0c3d414
IryArkhy <37odri@gmail.com>
2020-07-29 17:07:11 +0300

    Updates

---
+++ precache-manifest.8d5219656e5964235d2cb37807f043b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3989b5d95537c2cece9e38af2e4ce1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ec30baa358250837ce",
+    "url": "/static/css/main.3444bfbf.chunk.css"
+  },
+  {
+    "revision": "afdabee22cbf19366bbf",
+    "url": "/static/js/2.fbe938af.chunk.js"
+  },
+  {
+    "revision": "46ec30baa358250837ce",
+    "url": "/static/js/main.cdda10c9.chunk.js"
+  },
+  {
+    "revision": "7e099c92ca6b6755a063",
+    "url": "/static/js/runtime-main.9e8ceecc.js"
+  }
+]);
